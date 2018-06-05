bc0e249100c645c8bebe2a8b1b9a99a558794958
tjss <tjss@ic.ufal.br>
2018-06-04 23:21:58 -0300

    Att.

---
+++ temp.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define lwb lower_bound
+#define upb upper_bound
+#define all(E) E.begin(), E.end()
+#define e_min min_element
+#define e_max max_element
+#define bin_s binary_search
+#define ff find
+
+using namespace std;
+typedef long int ll;
+typedef vector<ll> vct;
+typedef vector< vct > vvct;
+typedef pair<ll, ll> ii;
+typedef pair<ll, ii> lii;
+typedef vector<ii> vii;
+typedef vector< vii > ADL;
+typedef bitset<1 << 20> VL;
+
+int main() {
+
+}
+
